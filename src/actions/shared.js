1f0c9303de8f86b94926639629ff34bf01667a48
virmon <virmontumulak@gmail.com>
2019-04-04 20:08:08 +0800

    feat: initialize data

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,10 +1,15 @@
 import { getInitialData } from '../utils/api'
-import { receiveTweets } from './tweets'
-import { receiveUsers } from './users'
-import { setAuthedUser } from './authedUser'
+import { receiveTweets } from '../actions/tweets'
+import { receiveUsers } from '../actions/users'
+import { setAuthedUser } from '../actions/authedUser'
 
-const AUTHED_USER = 'tyler_mcginnis'
+const AUTHED_USER = 'tylermcginnis'
 
+/** 
+ * thunk action creator
+ * Gets data from database and then dispatches actions to the 
+ * store to set the three pieces of state we have in our store
+ * */
 export function handleInitialData () {
     return (dispatch) => {
         return getInitialData()
