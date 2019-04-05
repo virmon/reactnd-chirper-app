8dae59f3da6d49aa629d8055de6710c42c477eb6
virmon <virmontumulak@gmail.com>
2019-04-05 19:00:25 +0800

    feat: add tweet and reply

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,4 @@
-import { RECEIVE_TWEETS, TOGGLE_TWEET } from '../actions/tweets'
+import { RECEIVE_TWEETS, TOGGLE_TWEET, ADD_TWEET } from '../actions/tweets'
 
 export default function tweets (state = {}, action) {
     switch(action.type) {
@@ -16,6 +16,42 @@ export default function tweets (state = {}, action) {
                         ? state[action.id].likes.filter((uid) => uid !== action.authedUser)
                         : state[action.id].likes.concat([action.authedUser])
                 }
+            }
+        case ADD_TWEET :
+            const { tweet } = action
+
+            let replyingTo = {}
+            if (tweet.replyingTo !== null) {
+                replyingTo = {
+                    [tweet.replyingTo]: {
+                        ...state[tweet.replyingTo],
+                        replies: state[tweet.replyingTo].replies.concat([tweet.id])
+                    }
+                }
+            }
+
+            /**
+             * Lesson Challenge
+             * Suppose we replaced the case ADD_TWEET: portion of the code in 
+             * the reducers/tweets file with the code below.
+             *  - Would the state change in the same way? Why or why not?
+             *  - Would the new tweet appear on the page? Why or why not?
+             */
+            // let replyingTo = {}
+            // if (tweet.replyingTo !== null) {
+            //    const allReplies = state[tweet.replyingTo].replies.concat([tweet.id]);
+         
+            //    return {
+            //    ...state,
+            //    [action.tweet.id]: action.tweet,
+            //    [action.tweet.replyingTo.replies]: allReplies
+            //    }
+            // }
+
+            return {
+                ...state,
+                [action.tweet.id]: action.tweet,
+                ...replyingTo,
             }
         default :
             return state
